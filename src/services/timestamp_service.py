65e049eb4b48267008f09574f3bc4b8bc32b6353
Niko Raulas <niko.raulas@outlook.com>
2026-03-02 13:49:45 +0200

    fix accidentally deleted files

---
+++ src/services/timestamp_service.py
@@ -0,0 +1,12 @@
+import datetime
+
+
+from db_module.dao.abstract import TimestampDAO
+
+class TimestampService:
+
+    def __init__(self, timestamp_dao: TimestampDAO):
+        self.timestamp_dao = timestamp_dao
+
+    def add_timestamp(self, time: datetime.datetime):
+        return self.timestamp_dao.create(time)
